c49b3c00a6c220896bf89536e0e15f28a0c72bf1
Novikov Denis <dn0500625@gmail.com>
2024-06-25 15:25:18 +0300

    добавлена сортировка товаров по имени и цене

--- client/src/components/ProfileComponents/MyOrders/MyOrders.jsx
+++ client/src/components/ProfileComponents/MyOrders/MyOrders.jsx
@@ -2,13 +2,16 @@
 import classes from './MyOrders.module.css'
 
 
-export default function MyOrders({children}){
+export default function MyOrders({children,sortOrders}){
     return (
 			<section className={classes.MyOrdersSection}>
 				<h2 className={classes.OrdersLogotype}>Мои заказы</h2>
-                <div className={classes.OrdersContainer}>
-                    {children}
-                </div>
+				<select className={classes.SelectSort} onChange={sortOrders}>
+					<option>Сортировка</option>
+					<option>Сортировать по цене</option>
+					<option>Сортировать по имени</option>
+				</select>
+				<div className={classes.OrdersContainer}>{children}</div>
 			</section>
 		)
 }
--- client/src/pages/profilePage/Profile.jsx
+++ client/src/pages/profilePage/Profile.jsx
@@ -3,39 +3,68 @@ import Order from '../../components/ProfileComponents/MyOrders/Order/order'
 import UserStats from '../../components/ProfileComponents/UserStats/UserStats'
 import UserInfo from '../../components/ProfileComponents/userInfo/UserInfo'
 import classes from './Profile.module.css'
+import { useState } from 'react'
 
 export default function Profile() {
+	const [orders, setOrders] = useState([
+		{
+			OrderName: 'в',
+			OrderPrice: 50000,
+			OrderStatus: 'куплен',
+		},
+		{
+			OrderName: 'а',
+			OrderPrice: 10000,
+			OrderStatus: 'куплен',
+			
+		},
+		{
+			OrderName: 'в',
+			OrderPrice: 5000,
+			OrderStatus: 'куплен',
+		},
+	])
+
+	function sortByPrice(prev) {
+		return [...prev].sort((a, b) => {
+			return a.OrderPrice - b.OrderPrice
+		})
+	}
+
+	function sortByName(prev) {
+		return [...prev].sort((a, b) => {
+			if (a.OrderName > b.OrderName) {
+				return 1
+			} else if (a.OrderName === b.OrderName) {
+				return 0
+			}
+			return -1
+		})
+	}
+
+	function sortOrders(e) {
+		if (e.target.value === 'Сортировать по цене') {
+			setOrders(prev => {
+				return sortByPrice(prev)
+			})
+		} else if (e.target.value === 'Сортировать по имени') {
+			setOrders(prev => sortByName(prev))
+		}
+	}
+
 	return (
 		<>
 			<div className={classes.ProfileContainer}>
 				<UserInfo />
-				<MyOrders>
-					<Order
-						OrderName='ноутбук клевый'
-						OrderPrice='50000'
-						OrderStatus='куплен'
-					/>
-					<Order
-						OrderName='ноутбук клевый'
-						OrderPrice='50000'
-						OrderStatus='куплен'
-					/>
-					<Order
-						OrderName='ноутбук клевый'
-						OrderPrice='50000'
-						OrderStatus='куплен'
-					/>
-					<Order
-						OrderName='ноутбук клевый'
-						OrderPrice='50000'
-						OrderStatus='куплен'
-					/>
-
-					<Order
-						OrderName='ноутбук клевый'
-						OrderPrice='50000'
-						OrderStatus='куплен'
-					/>
+				<MyOrders sortOrders={sortOrders}>
+					{orders.map((order,index) => (
+						<Order
+							key={index}
+							OrderName={order.OrderName}
+							OrderPrice={order.OrderPrice}
+							OrderStatus={order.OrderStatus}
+						/>
+					))}
 				</MyOrders>
 				<UserStats
 					returnSum={17896}
